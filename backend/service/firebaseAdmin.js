ae28f61531ce75c2a040804f84e2e1cb237068fc
HarshVardhan <ht94171@gmail.com>
2023-12-27 01:04:37 +0530

    fixing firebase email auth and some typo errors

--- backend/service/firebaseAdmin.js
+++ backend/service/firebaseAdmin.js
@@ -2,11 +2,8 @@ const admin = require('firebase-admin');
 
 const serviceAccount = require('./vit-marketplace-adminSdk.json');
 
-const databaseURL = 'https://console.firebase.google.com/project/vit-marketplace-5c31b/database/vit-marketplace-5c31b-default-rtdb/data/~2F';
-
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: databaseURL,
 });
 
 const db = admin.firestore();
